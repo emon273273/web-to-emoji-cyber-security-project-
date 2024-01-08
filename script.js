b2cb8e8fc84536b4ebbaf5cb70f9433fa7aeb36d
emon273273 <emon.cse273@gmail.com>
2024-01-08 14:04:21 +0600

    cyber project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let emon=""
 
+let found;
 function encryption(){
 
   document.querySelector("#encrypt-btn").addEventListener("click",function(){
@@ -12,7 +13,7 @@ function encryption(){
      //input ke array
 
      let str=input.split("")
-     let str1=pass.split("")
+   
 //  let str3=str.concat(str1);
  
 //     console.log(str1)
@@ -24,9 +25,9 @@ function encryption(){
 
      str.forEach(element=>{
 
-      emon+=`&#128${element.charCodeAt()}`
+      emon+=`&#128${element.charCodeAt()} `
      })
-     console.log(emon)
+    //  console.log(emon)
 
 //storing in #result
     
@@ -39,7 +40,7 @@ function encryption(){
 
         dataarr=JSON.parse(localStorage.getItem("data1"))
 
-        dataarr.push=({"pass":pass,"input":input,"emon":emon})
+        dataarr.push({"pass":pass,"input":input,"emon":emon})
       }
       else{
 
@@ -59,68 +60,95 @@ function encryption(){
  
 }
 
-encryption()
+
 
 //Encrypt theke decrept page a neya ba decrypt theeke encrypt a neya
+
+function decryption(){
+
+  document.querySelector("#decrypt-btn").addEventListener("click",function(){
+
+    let emon2=""
+    //getting emoji msg
+    let input2=document.querySelector("#emojimsg").value
+      //getting final password
+    let password2=document.querySelector("#finalpassword").value
+
+    let  user=JSON.parse(localStorage.getItem('data1'))
+
+    // console.log(user)
+
+    let str2=input2.split(" ")
+
+   str2.forEach(e=>{
+    emon2+=`&#${(e.codePointAt(0))} `
+
+   });
+
+    console.log(emon2)
+
+let found;
+
+    for(let i of user){
+
+      if(i.emon===emon2){
+      found=i;
+    console.log(i);
+      }
+    }
+
+    if(found.emon===emon2 && found.pass==password2){
+
+      document.querySelector("#result").style.display="block"
+      document.querySelector("#result").style.color="#eee"
+
+      document.querySelector("#result").innerHTML=found.input;
+
+    }else{
+      document.querySelector("#result").style.display="block"
+      document.querySelector("#result").style.color="red"
+      document.querySelector("#result").innerHTML="wrong  password";
+    }
+
+
+
+
+
+
+
+
+  })
+}
 
 
 
 function btnClicking() {
 
-    //decryption button
+  document.querySelector("button").addEventListener("click", function () {
+      document.querySelector("#result").style.display = "block"
+      // console.log(localStorage.getItem("password"))
+      // console.log(localStorage.getItem("emojis"))
+  })
   document.querySelector("#dec-btn").addEventListener("click", function () {
-    document.querySelector("#decryption").style.display = "block";
-
-    document.querySelector("#encryption").style.display= "none";
-
-    //color change korlam
-
-    document.querySelector("#dec-btn").style.backgroundColor="#333";
-    document.querySelector("#enc-btn").style.backgroundColor="#222";
-
-    //arrow niye kaj 
-
-    document.querySelector("#main >h1 span img").style.rotate="270deg";
-  });
-
-
-
-//encryption button 
+      document.querySelector("#result").style.display = "none"
+      document.querySelector("#decryption").style.display = "block"
+      document.querySelector("#encryption").style.display = "none"
+      document.querySelector("#dec-btn").style.backgroundColor = "#333"
+      document.querySelector("#enc-btn").style.backgroundColor = "#222"
+      document.querySelector("#main>h1 span img").style.rotate = '270deg'
+  })
   document.querySelector("#enc-btn").addEventListener("click", function () {
-    document.querySelector("#encryption").style.display= "block";
+      document.querySelector("#decryption").style.display = "none"
+      document.querySelector("#result").style.display = "none"
+      document.querySelector("#encryption").style.display = "block"
+      document.querySelector("#dec-btn").style.backgroundColor = "#222"
+      document.querySelector("#enc-btn").style.backgroundColor = "#333"
+      document.querySelector("#main>h1 span img").style.rotate = '90deg'
 
-    document.querySelector("#decryption").style.display= "none";
-
-    //color change 
-    document.querySelector("#dec-btn").style.backgroundColor="#222";
-    document.querySelector("#enc-btn").style.backgroundColor="#333";
-    document.querySelector("#main >h1 span img").style.rotate="90deg";
-    document.querySelector("#result").style.display="none"
-  });
-
-  document.querySelector("#encrypt-btn").addEventListener("click",function(){
-
-    document.querySelector("#result").style.display="block"
-  })
-
-
-
-  //decrypt button a click korle
-
-  document.querySelector("#enc-btn").addEventListener("click",function(){
-
-    document.querySelector("#result").style.display="block"
-  })
-
-
-  document.querySelector("#dec-btn").addEventListener("click",function(){
-
-    document.querySelector("#result").style.display="block"
   })
 }
  btnClicking() 
 
+ encryption()
+ decryption();
  //local storage niye kaj
-
- let arr=
-localStorage.getItem("")
